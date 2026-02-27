9ac6e0ba7b023d5b4ac7f4b6546d4ce1b85c1ca8
DESKTOP-DHEBCDJ\andre <andreslopez>
2026-02-27 10:53:54 -0500

    Countries

--- Frontend/src/constants/countries.js
+++ Frontend/src/constants/countries.js
@@ -1,6 +1,7 @@
 async function fetchCountries() {
   try {
-    const response = await fetch("https://restcountries.com/v3.1/all");
+    const response = await fetch("https://restcountries.com/v3.1/all?fields=name,flags");
+    // const response = await fetch("https://restcountries.com/v3.1/all");
 
     if (!response.ok) throw new Error("Ocurrió un error al obtener los países");
 
